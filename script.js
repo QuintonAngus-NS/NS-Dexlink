e9a90a60996b03a6c47d9c536b0e386bb5c74d44
quintonangus04-afk <quintonangus04@gmail.com>
2026-03-07 14:03:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ submitBtn.addEventListener('click', async (e) => {
     window.location.href = 'https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=GiIF7R1az0aDCTZGiqcMzRCem88RN2vk&redirect_uri=https://dexlink-server-808702537090.europe-west1.run.app/callBack&response_type=code&scope=offline_access'
 
 })
+
+const params = new URLSearchParams(window.location.search);
+
+if (params.get("loggedIn") === "true") {
+    // Hide login screen
+    detailsWrapper.style.display = "none";
+
+    // Show loading screen
+    loadingWrapper.style.display = "flex";
+
+    // Start fetching glucose data
+    fetchGlucoseData();
+}
