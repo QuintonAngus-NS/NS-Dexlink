0569597bef1e80323de6c5f1add3864924294c90
quintonangus04-afk <quintonangus04@gmail.com>
2026-03-10 17:28:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ submitBtn.addEventListener('click', (e) => {
 
     // EU PRODUCTION LOGIN URL
     window.location.href =
-        'https://api.dexcom.eu/v2/oauth2/login' +
-        '?client_id=ctT7YimKIZHQ4cC7g9j4EWtq7MQVR63y' +
-        '&redirect_uri=https://dexlink-server-808702537090.europe-west1.run.app/callBack' +
-        '&response_type=code' +
-        '&scope=offline_access';
+    "https://api.dexcom.eu/v2/oauth2/login" +
+    "?client_id=ctT7YimKIZHQ4cC7g9j4EWtq7MQVR63y" +
+    "&redirect_uri=https://dexlink-server-808702537090.europe-west1.run.app/callBack" +
+    "&response_type=code" +
+    "&scope=offline_access";
+
 });
 
 // --- FETCH GLUCOSE DATA FROM BACKEND ---
